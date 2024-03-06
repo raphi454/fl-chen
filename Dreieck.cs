5998acf6d05b2c8f835311b656da3ab3ad6b4d1f
rsteger <rsteger@schueler.hakzell.at>
2024-03-06 10:09:42 +0100

    add dreieck

---
+++ Dreieck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flaechen;
+
+public class Dreieck : Fläche
+{
+    double _g;
+    double _h;
+    public Dreieck(double g, double h)
+    {
+        this._g = g;
+        this._h = h;
+    }
+    public override double GetFläche()
+    {
+        return _g * _h / 2;
+    }
+}
