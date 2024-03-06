ebaa4e1ed21ebf267209ab0d11bb0333ed3f7f36
rsteger <rsteger@schueler.hakzell.at>
2024-03-06 10:15:12 +0100

    add Gesamtfläche

---
+++ Gesamtfläche.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flaechen;
+public class Gesamtfläche
+{
+    List<Fläche> _flächen = new();
+
+    public void Add(Fläche fläche)
+    {
+        _flächen.Add(fläche);
+    }
+    public double GetFläche()
+    {
+        double sum = 0.0;
+
+        for (int i = 0; i < _flächen.Count; i++)
+        {
+            sum = sum + _flächen[i].GetFläche();
+        }
+
+        return 0;
+    }
+}
